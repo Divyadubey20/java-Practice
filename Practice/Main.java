ed6c314b806c70d21444c0e7f52771daa74f0fa7
Divya <divyadubey20102006@gmail.com>
2026-06-09 11:21:41 +0530

    Commit1

---
+++ Practice/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentance you want to count vovels in: ");
+        String v = sc.nextLine();
+        int count = 0;
+        
+        for(int i = 0; i<v.length(); i++){
+            v = v.toUpperCase();
+            if(v.charAt(i)=='A' || v.charAt(i)=='E' || v.charAt(i)=='I' || v.charAt(i)=='O' || v.charAt(i)=='U'){
+                count++;
+            }
+        }System.out.println(count);
+    }
+}
+
